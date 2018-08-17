86502f862bf8f8a4bc36d0ac89e45f10588a3def
Your Name <you@example.com>
2018-08-17 03:37:24 -0700

    0817

---
+++ include/player_notiy.h
@@ -0,0 +1,12 @@
+//
+// Created by bingo on 8/17/18.
+//
+
+#ifndef PLAYER_PLAYER_NOTIY_H
+#define PLAYER_PLAYER_NOTIY_H
+
+#include <glib.h>
+#include "protocol.h"
+
+#endif //PLAYER_PLAYER_NOTIY_H
+
